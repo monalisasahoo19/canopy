afe4a515363b5b02e797777c5d9a83e523f28894
Monalisa Sahoo <monalisasahoo19@gmail.com>
2017-09-17 23:32:09 +1000

    Code refactoring

---
+++ e2e/selectors/homePageSelectors.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testInputField: '@testInputField1',
+    ajaxField: '@ajaxField',
+    firstDropdown: '@firstDropdown',
+    item2: 'option[value=\'2\']',
+    clickMeButton: '@clickMeButton',
+    buttonClickActionResult: '@buttonClickActionResult',
+    alertHyperLink: '@alertHyperLink',
+    statesOptions: '@statesOptions'
+};
--- e2e/tests/home.js
+++ e2e/tests/home.js
@@ -1,5 +1,14 @@
-// see: http://nightwatchjs.org/ for docs.
 let assert = require('assert');
+
+// homePageSelector contains the CSS Selector of the DOM elements
+let homePageSelectors = require('./../selectors/homePageSelectors');
+
+/** 
+  * expectedHomePage contains the expected test data for the home page test
+  * Having test data in separate file is quite neat and easier to maintain
+   as it can be shared across multiple file.
+*/
+let expectedResult = require('./../data/expectedResultHomePage');
 
 let home;
 
@@ -10,69 +19,65 @@ module.exports = {
   },
 
   'Should have title: "canopy testing page"': () => {
-    home.assert.title('canopy testing page');
+
+    home.assert.title(expectedResult.title);
   },
 
   'Should be able to enter data to Text field 1,': () => {
-    
-    let testInputField = '@testInputField1';
 
-    home.verify.value(testInputField, 'test value 1')
+    let testInputField = homePageSelectors.testInputField;
+
+    home.verify.value(testInputField, expectedResult.testValue1)
       .clearValue(testInputField)
-      .setValue(testInputField, 'test value X')
-      .verify.value(testInputField, 'test value X');
+      .setValue(testInputField, expectedResult.testValueX)
+      .verify.value(testInputField, expectedResult.testValueX);
   },
 
   'Should display "ajax loaded" message': () => {
-    let ajaxSelector = '@ajaxField';
-    let expectedResult = 'ajax loaded';
 
-    home.expect.element(ajaxSelector).text.to.equal('ajax loaded').after(2000);
+    home.expect.element(homePageSelectors.ajaxField)
+      .text.to.equal(expectedResult.ajaxLoadedMessage)
+      .after(2000);
   },
 
   'Should be able to select "Item 2" in the first dropdown': () => {
 
-    let firstDropdownSelector = '@firstDropdown';
+    let firstDropdownSelector = homePageSelectors.firstDropdown;
 
+    //before hook is added to esnure the prerequisite has happened prior to the execution of assertion
     before: () => {
       home.click(firstDropdownSelector, () => {
-        home.click("option[value='2']");
+        home.click(homePageSelectors.firstDropdownItem2);
       });
     }
-
-    home.expect.element(firstDropdownSelector).to.contain.text('Item 2');
+    home.expect.element(firstDropdownSelector).to.contain.text(expectedResult.item2);
   },
 
   'When button "Click Me!!" clicked, should have text "button clicked"': () => {
 
-    let clickMeButtonSelector = '@clickMeButton';
-    let buttonClickActionResult = '@buttonClickActionResult';
-
-    home.click(clickMeButtonSelector)
-      .verify.containsText(buttonClickActionResult, 'button clicked');
+    home.click(homePageSelectors.clickMeButton)
+      .verify.containsText(homePageSelectors.buttonClickActionResult, expectedResult.buttonClickedMessage);
   },
 
   'When "Alert Me!" link clicked, should display alert.': (client) => {
-    let alertHyperLink = '@alertHyperLink';
 
-    home.click(alertHyperLink);
+    home.click(homePageSelectors.alertHyperLink);
+
     client.getAlertText((result) => {
-      assert.equal(result.value, "test!");
+      //Alert text is validated to ensure the alert message is being shown
+      assert.equal(result.value, expectedResult.testAlertMessage);
       client.acceptAlert();
     });
   },
 
-  'Should have 70 states options in "#states" dropdown select.': (client) => {
+  'Should have 70 states options in "#states" dropdown select.': () => {
 
-    let statesOptions = '@statesOptions';
-
-    home.assert.elementCount(statesOptions, 70)
-    // client.elements('css selector','select#states > option', function (result) {
-    //   client.assert.equal(result.value.length, 70);
-    // });
+    //elementCount is a re-usable the nightwatch assertion extension (http://nightwatchjs.org/guide#extending)
+    //The code for the custom assertions can be found at e2e/assertions/elementCount
+    home.assert.elementCount(homePageSelectors.statesOptions, expectedResult.statesCount);
   },
+
   after: (client) => {
     client.end();
   }
 };
-

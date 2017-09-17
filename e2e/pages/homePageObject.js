3bc37de859f83189d25a66c1e7968cc45e75b7e7
Monalisa Sahoo <monalisasahoo19@gmail.com>
2017-09-17 12:02:07 +1000

    Tests are added

--- e2e/pages/homePageObject.js
+++ e2e/pages/homePageObject.js
@@ -1,6 +1,27 @@
 module.exports = {
   elements: {
     title: 'title',
+    testInputField1:{
+      selector:'#testfield1'
+    },
+    ajaxField:{
+      selector:'#ajax'
+    },
+    firstDropdown:{
+      selector:'select#item_list'
+    },
+    clickMeButton:{
+      selector:'#button' // you also could input[id="button"]
+    },
+    buttonClickActionResult:{
+      selector:'#button_clicked' // you also could input[id="button"]
+    },
+    alertHyperLink:{
+      selector:'a#alert' 
+    },
+    statesOptions:{
+      selector:'select#states > option' 
+    }
   },
   commands: [{
     goToHomepage() {
--- e2e/tests/home.js
+++ e2e/tests/home.js
@@ -1,4 +1,5 @@
 // see: http://nightwatchjs.org/ for docs.
+let assert = require('assert');
 
 let home;
 
@@ -7,28 +8,68 @@ module.exports = {
     home = client.page.homePageObject();
     home.goToHomepage();
   },
-  
-  // Example test case
+
   'Should have title: "canopy testing page"': () => {
     home.assert.title('canopy testing page');
   },
+
   'Should be able to enter data to Text field 1,': () => {
+    
+    let testInputField = '@testInputField1';
 
+    home.verify.value(testInputField, 'test value 1')
+      .clearValue(testInputField)
+      .setValue(testInputField, 'test value X')
+      .verify.value(testInputField, 'test value X');
   },
+
   'Should display "ajax loaded" message': () => {
+    let ajaxSelector = '@ajaxField';
+    let expectedResult = 'ajax loaded';
 
+    home.expect.element(ajaxSelector).text.to.equal('ajax loaded').after(2000);
   },
+
   'Should be able to select "Item 2" in the first dropdown': () => {
 
+    let firstDropdownSelector = '@firstDropdown';
+
+    before: () => {
+      home.click(firstDropdownSelector, () => {
+        home.click("option[value='2']");
+      });
+    }
+
+    home.expect.element(firstDropdownSelector).to.contain.text('Item 2');
   },
+
   'When button "Click Me!!" clicked, should have text "button clicked"': () => {
 
-  },
-  'When "Alert Me!" link clicked, should display alert.': () => {
+    let clickMeButtonSelector = '@clickMeButton';
+    let buttonClickActionResult = '@buttonClickActionResult';
 
+    home.click(clickMeButtonSelector)
+      .verify.containsText(buttonClickActionResult, 'button clicked');
   },
-  'Should have 70 states options in "#states" dropdown select.': () => {
 
+  'When "Alert Me!" link clicked, should display alert.': (client) => {
+    let alertHyperLink = '@alertHyperLink';
+
+    home.click(alertHyperLink);
+    client.getAlertText((result) => {
+      assert.equal(result.value, "test!");
+      client.acceptAlert();
+    });
+  },
+
+  'Should have 70 states options in "#states" dropdown select.': (client) => {
+
+    let statesOptions = '@statesOptions';
+
+    home.assert.elementCount(statesOptions, 70)
+    // client.elements('css selector','select#states > option', function (result) {
+    //   client.assert.equal(result.value.length, 70);
+    // });
   },
   after: (client) => {
     client.end();
